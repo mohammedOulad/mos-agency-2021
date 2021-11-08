4bd828d08429024e70c25403c0ea09664c405ffc
mohammed oulad mhand <mohammed.ouladmhand@student.ap.be>
2021-11-09 00:25:07 +0100

    artists displayen

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,50 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
-import { container } from './layout.module.css'
+import { Link, useStaticQuery, graphql } from 'gatsby'
+import { 
+  container, 
+  nav, 
+  navLinks, 
+  navLinkItem, 
+  navLinkText, 
+  siteTitle 
+} from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
   return (
     <div className={container}>
-      <title>{pageTitle}</title>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
+      <title>{pageTitle} | {data.site.siteMetadata.title}</title>
+      <nav className={nav}>
+        <header className={siteTitle}>
+          {data.site.siteMetadata.title}
+        </header>
+        <ul className={navLinks}>
+        <li>
+        </li>
+          <li className={navLinkItem}>
+            <Link className={navLinkText} to="/">
+              Home
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link className={navLinkText} to="/about">
+              About
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link className={navLinkText} to="/artists">
+              Artists
+            </Link>
+          </li>
         </ul>
       </nav>
       <main>
